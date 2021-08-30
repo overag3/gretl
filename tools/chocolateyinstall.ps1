6660b6387222bc48b11b2651f75f6b9399555dbd
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2021-08-30 15:49:43 +0200

    update to version 2021.210829

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://prdownloads.sourceforge.net/gretl/gretl-2021a-32.exe'
-$url64 = 'https://prdownloads.sourceforge.net/gretl/gretl-2021a-64.exe'
+
+$url = 'https://prdownloads.sourceforge.net/gretl/gretl-2021c-32.exe'
+$url64 = 'https://prdownloads.sourceforge.net/gretl/gretl-2021c-64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  unzipLocation  = $toolsDir
   fileType       = 'EXE'
   url            = $url
   url64bit       = $url64
   softwareName   = 'gretl*'
-  checksum       = 'ECA9F21D123EBEA64768944636F94ECF4244F2177E4B3F07D323723135827098'
+  checksum       = 'F56D872AA50B6CC6A7E11590770E1C675700D3D8F5093B2316AE9C8EEC366958'
   checksumType   = 'sha256'
-  checksum64     = 'CFA60BF1ED57491B542B680F9F97807AE8F0C7BA1B72BD1322CF88AFFD439C7C'
+  checksum64     = '780F69FB24E0944DA2F7E6C1C76739F95CC5F6D9905AB0442CFD25875EE487DA'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
